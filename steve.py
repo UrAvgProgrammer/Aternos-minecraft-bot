bdfa7c12abcaa9942f949f99433391f9a1b3dd6f
Jerald Talledo <jrldtlld@gmail.com>
2021-08-08 22:07:37 +0800

    added aternosAPI

--- steve.py
+++ steve.py
@@ -1,33 +1,32 @@
 import os
 import discord
-from tasks import start_mc_server
+from aternosapi import AternosAPI
 
 client = discord.Client()
 
+ATERNOS_SESSION=
+HEADERS = server = AternosAPI(HEADERS, ATERNOS_SESSION)
+
+
 @client.event
 async def on_ready():
-  print('Bot ready')
+    print('Bot ready')
 
 @client.event
 async def on_message(msg):
-  if msg.author == client.user:
-    return 0
+    if msg.author == client.user:
+        return 0
 
-  if msg.content.lower() == '?steve start server':
-    # start server here
-    await msg.channel.send(start_server())
+    if msg.content.lower() == '?steve start server':
+        await msg.channel.send(server.StartServer())
   
-  if msg.content.lower() == '?steve restart server':
-    # restart server here
-    await msg.channel.send('steve restart server checked')
+    if msg.content.lower() == '?steve stop server':
+        await msg.channel.send(server.StopServer())
 
-  if msg.content.lower() == '?steve give ip':
-    # give server info
-    await msg.channel.send('steve give ip checked')
+    if msg.content.lower() == '?steve give server info':
+        await msg.channel.send(server.GetServerInfo())
 
-# main functions here
+    if msg.content.lower() == '?steve give server status':
+        await msg.channel.send(server.GetStatus())
 
-def start_server():
-  return start_mc_server()
- 
 client.run(os.getenv('TOKEN'))
