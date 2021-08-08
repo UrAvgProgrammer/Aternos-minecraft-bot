513c59b14972ea63bf0b004cedd7a965f7d4c06f
UrAvgEngineer <31199549+UrAvgProgrammer@users.noreply.github.com>
2021-08-08 10:49:23 +0000

    added initial selenium code TODO: Add method to bypass initial captcha protection.

--- steve.py
+++ steve.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from tasks import start_mc_server
 
 client = discord.Client()
 
@@ -7,15 +8,14 @@ client = discord.Client()
 async def on_ready():
   print('Bot ready')
 
-
-
+@client.event
 async def on_message(msg):
   if msg.author == client.user:
     return 0
-  
+
   if msg.content.lower() == '?steve start server':
     # start server here
-    await msg.channel.send('?steve start server')
+    await msg.channel.send(start_server())
   
   if msg.content.lower() == '?steve restart server':
     # restart server here
@@ -24,6 +24,10 @@ async def on_message(msg):
   if msg.content.lower() == '?steve give ip':
     # give server info
     await msg.channel.send('steve give ip checked')
-  
 
-client.run(os.environ['TOKEN'])
+# main functions here
+
+def start_server():
+  return start_mc_server()
+ 
+client.run(os.getenv('TOKEN'))
